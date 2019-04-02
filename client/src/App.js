08860d9121358d4dc4299b0e5870389a20da5da4
ilavisharma <lavisharma796@gmail.com>
2019-04-02 22:43:21 +0530

    add spacex logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SpaceX</h1>
+        <img
+          src={logo}
+          alt="spaceX logo"
+          style={{ width: 300, display: 'block', margin: 'auto' }}
+        />
       </div>
     );
   }
